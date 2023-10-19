f0d72860eaa9372bc30507e4b838f409cc998204
Thomas Loo <tom@TomsPC>
2023-10-20 03:21:36 +1100

    Create Menu and Dialogue classes to tidy up code

--- main.py
+++ main.py
@@ -25,22 +25,15 @@ class BeyBlade:
     def get_total_stats(self):
         return round(self.strength * self.strength_modifier + self.speed * self.speed_modifier + self.stamina * self.stamina_modifier)
 
-    def present_beyblade(self):
-        delay_print(f"Here is your new BeyBlade ~ {self.name.capitalize()} ~ with a total power of {self.total_stats}!\n")
-        if self.strength_modifier > self.speed_modifier and self.strength_modifier > self.stamina_modifier:
-            delay_print(f"It appears that your BeyBlade favours STRENGTH upgrades!\n")
-        elif self.speed_modifier > self.stamina_modifier:
-            delay_print(f"It appears that your BeyBlade favours SPEED upgrades!\n")
-        else:
-            delay_print(f"It appears that your BeyBlade favours STAMINA upgrades!\n")
+
 
 class Upgrades:
     # Show upgrades and cost
     shop_visit = 1
-    strength_random_price = random.randint(12, 48)
-    speed_random_price = random.randint(12, 48)
-    stamina_random_price = random.randint(12, 48)
-    
+    strength_random_price = random.randint(12, 30)
+    speed_random_price = random.randint(12, 30)
+    stamina_random_price = random.randint(12, 30)
+
     def show_upgrades():
         print(f''' ** Welcome to the UPGRADES shop! **
 [A] Strength stat upgrade: {Upgrades.strength_random_price} dollars
@@ -60,52 +53,64 @@ You can only upgrade once before each battle!''')
         else:
             print("You don't have any upgrade slots available!")
 
-def menu():
-    while player.playing == True:
-        hud()
-        choice = input("")
-        if choice == "1":
-            print(f"Your BeyBlade stats:\nStrength: {player.strength}\nSpeed: {player.speed}\nStamina: {player.stamina}\n\n(Stat modifiers are hidden)\nYou have one {player.upgrades_count} upgrade slot available!\n")
-        elif choice == "2":
-            if Upgrades.shop_visit > 0:
-                Upgrades.show_upgrades()
-            else:
-                print("You already visited the shop!")
-        elif choice == "3":
-            pass
-        elif choice == "A":
-            Upgrades.buy_upgrade(choice)
+class Dialogue:
+    def intro():
+        welcome_message = "Welcome to the 2023 Battle BeyBlade Bonanza!\n"
+        delay_print(welcome_message)
+        # print()
+        delay_print("Before we get started, could we please have your name for registration?\n")
+        player_name = input()
+        delay_print(f"Thank you for registering {player_name}! We are so glad to have you here!\nAs per the tournament rules, you will be renting one of our Tournament BeyBlades!\n")
 
+    def name_beyblade():
+        delay_print("What would you like to name your BeyBlade?\n")
 
+    def present_beyblade(self):
+        delay_print(f"Here is your Tournament BeyBlade ~ {self.name.capitalize()} ~ with a total power of {self.total_stats}!\n")
+        if self.strength_modifier > self.speed_modifier and self.strength_modifier > self.stamina_modifier:
+            delay_print(f"It appears that your BeyBlade favours STRENGTH upgrades!\n")
+        elif self.speed_modifier > self.stamina_modifier:
+            delay_print(f"It appears that your BeyBlade favours SPEED upgrades!\n")
+        else:
+            delay_print(f"It appears that your BeyBlade favours STAMINA upgrades!\n")
+    
+    def beyblade_stats():
+        print(f"Your BeyBlade stats:\nStrength: {player.strength}\nSpeed: {player.speed}\nStamina: {player.stamina}\n\n(Stat modifiers are hidden)\nYou have one {player.upgrades_count} upgrade slot available!\n")
+
+class Menu:
+    def menu():
+        while player.playing == True:
+            Menu.hud()
+            choice = input("")
+            if choice == "1":
+                Dialogue.beyblade_stats()
+            elif choice == "2":
+                if Upgrades.shop_visit > 0:
+                    Upgrades.show_upgrades()
+                else:
+                    print("You already visited the shop!")
+            elif choice == "3":
+                pass
+            elif choice == "A":
+                Upgrades.buy_upgrade(choice)
+
+    def hud():
+        print(f'''===================================================================
+Rounds left: {player.rounds_to_play}   |   Total BeyBlade power: {player.get_total_stats()}   |   Money: {player.money}''')
+        print(f'''[1] Check BeyBlade Stats | [2] Go to Upgrade Store | [3] Battle
+===================================================================''') 
+    
 def delay_print(s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.01)
 
-def hud():
-    print(f'''
-===================================================================
-Rounds left: {player.rounds_to_play}   |   Total BeyBlade power: {player.get_total_stats()}   |   Money: {player.money}''')
-    print(f'''[1] Check BeyBlade Stats | [2] Go to Upgrade Store | [3] Battle
-===================================================================\n''')    
-
-def intro():
-    welcome_message = "Welcome to the 2023 Battle BeyBlade Bonanza!\n"
-    delay_print(welcome_message)
-    # print()
-    delay_print("Before we get started, could we please have your name for registration?\n")
-    player_name = input()
-    delay_print(f"Thank you for registering {player_name}! We are so glad to have you here!\n")
-
-def name_beyblade():
-    delay_print("What would you like to name your BeyBlade?\n")
-
 # Main
-intro()
-name_beyblade()
+Dialogue.intro()
+Dialogue.name_beyblade()
 player = BeyBlade(input())
-player.present_beyblade()
-menu()
+Dialogue.present_beyblade(player)
+Menu.menu()
 
 
